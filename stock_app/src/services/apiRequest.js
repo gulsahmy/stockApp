5adca1e6f301c31acc14ec89e37f51cea4a5a53e
gulsah <gmutluayyigit@gmail.com>
2024-12-18 15:22:24 +0300

    fethFail

--- stock_app/src/services/apiRequest.js
+++ stock_app/src/services/apiRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify"
-import { fetchStart } from "../features/authSlice"
+import { fetchFail, fetchStart, loginSuccess } from "../features/authSlice"
 import { useDispatch } from "react-redux"
 
 
@@ -17,6 +17,7 @@ export const login = async (userData) => {
         console.log(data)
         return data
     } catch (error) {
+        dispatch(fetchFail())
         toastErrorNotify("Login işlemi başarısız")
         console.log(error)
         
